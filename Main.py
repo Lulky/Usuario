3f1bfe71923ea3dd2c9b6d06b5850968018426df
Lulky <lukyluxiano.12@gmail.com>
2022-03-02 15:31:45 -0500

    changes

--- Main.py
+++ Main.py
@@ -3,17 +3,36 @@ from User import User
 
 john = User("jhon")
 mary = User("Mary")
+luciano = User("Luciano")
 
 
 
 john.hacer_deposito(1500)
-john.hacer_retiro(100)
-
-mary.hacer_deposito(100)
-
-john.transfer_dinero(mary, 100)
+john.hacer_deposito(300)
+john.hacer_deposito(200)
+john.hacer_retiro(600)
 
 john.mostrar_balance_usuario()
 
-print(john.pesos)
-print(mary.pesos)
+mary.hacer_deposito(400)
+mary.hacer_deposito(500)
+
+mary.mostrar_balance_usuario()
+
+luciano.hacer_deposito(400)
+luciano.hacer_retiro(50)
+luciano.hacer_retiro(150)
+luciano.hacer_retiro(199)
+
+luciano.mostrar_balance_usuario()
+
+#Bonus
+
+john.transfer_dinero(luciano, 400)
+
+john.mostrar_balance_usuario()
+luciano.mostrar_balance_usuario()
+
+
+
+
--- User.py
+++ User.py
@@ -4,11 +4,11 @@ class User:
         self.nombre = nombre
         self.pesos = 0
     
-    def hacer_retiro(self, cantidad):
-        self.pesos -= cantidad
-
     def hacer_deposito(self, cantidad):
         self.pesos += cantidad
+    
+    def hacer_retiro(self, cantidad):
+        self.pesos -= cantidad
 
     def mostrar_balance_usuario(self):
         print(f"Usuario: {self.nombre}, Balance: {self.pesos}")
